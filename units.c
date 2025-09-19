d941a80cc3f1589b3270057a9cdb8e07968fea1f
74clinton <babraclinton4@gmail.com>
2025-09-19 13:09:08 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,21 @@
+// Display of All Units This Semester
+/*
+Name: Clinton Ochieng
+Reg no:PA106/G/28797/25
+Description: Program to display all units for the  semester
+*/
+
+#include <stdio.h>
+int main() {
+	printf("SPM 2100 Discrete of Mathematics \n");
+	printf ("SPM 2104 Mathematics for Science \n");
+	printf ("UCU 2105 Fundamentals of ICT \n");
+	printf ("SPI 2102 Computer System Organization \n");
+	printf ("UCU 2101 Communication Skills \n");
+	printf ("SPC 2101 Introduction to Programming & Probem Solving \n");
+	printf ("2101 Fundamentals of System Analysis & Design \n");
+	
+	return 0;
+}                  
+  
+  	
