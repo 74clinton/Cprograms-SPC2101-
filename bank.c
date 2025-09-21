d58dac0be1b9c4774d9db984a90b325a34933fb6
74clinton <babraclinton4@gmail.com>
2025-09-21 10:36:30 +0300

    Add files via upload

---
+++ bank.c
@@ -0,0 +1,27 @@
+/*
+ Name: Clinton Ochieng
+ Reg No:PA106/G/28797/25
+ Description: Bank accont personal details
+ */
+ 
+ #include <stdio.h>
+ int main () {
+ 	int Height;
+ 	int IDNumber;
+ 	float Bankbalance;
+ 	
+ 	printf ("Enter your height in metres: \n");
+ 	scanf ("%f", &Height);
+ 	printf ("Your Height is %d \n" ,Height);
+ 	
+ 	printf("Enter your IDNumber: \n");
+ 	scanf ("%d",&IDNumber);
+ 	printf ("Your IDNumber is %d \n",IDNumber);
+ 	
+ 	printf ("Enter your Bankbalance: \n");
+ 	scanf("%f" ,&Bankbalance);
+ 	printf("Your Bankbalance is ksh.% \n", Bankbalance);
+ 	
+ 	return 0;
+ }
+     
